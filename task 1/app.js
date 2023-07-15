d1d3d3604b9b12d7d95fc1c52418c06ecb3d1686
Hasnaa Soliman <74362843+H-Shourab@users.noreply.github.com>
2023-07-15 19:33:49 +0300

    Add files via upload

---
+++ task 1/app.js
@@ -0,0 +1,5 @@
+const maths=require("./maths")
+console.log("sum :",maths.sum(2,2))
+console.log("subtract :",maths.subtract(2,2))
+console.log("multiply :",maths.multiply(2,2))
+console.log("division :",maths.division(2,2))
